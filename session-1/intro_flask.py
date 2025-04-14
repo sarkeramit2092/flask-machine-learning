3c576095ed043ff07a665252cd4a4d27fb4c0849
cloud09 <91081537+sarkeramit2092@users.noreply.github.com>
2025-04-14 23:56:24 +0600

    Add files via upload

---
+++ session-1/intro_flask.py
@@ -0,0 +1,14 @@
+# endpoint it's an address of a web page
+# Like: https://www.abc.com
+# Like: https://www.abc.com/about
+
+# path parameters are used for accessing a particular resource at an endpoint
+
+# endpoint >> https://www.abc.com/employees
+# path parameters >> https://www.abc.com/employees/4 (4 let assume employees ID)
+
+# query parameters are used for filtering the data received from an endpoint
+
+# query parameters >> https://www.abc.com/employees?dep=sales&loc=dhaka
+
+# POST GET PUT DELETE ..... Create Retrieve Update Delete
