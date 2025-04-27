e03b6dfe63f4b6161424b11f09b3b8aa38544c2f
cloud09 <91081537+sarkeramit2092@users.noreply.github.com>
2025-04-27 14:35:42 +0600

    url redirection

--- session-2/url_redirection.py
+++ session-2/url_redirection.py
@@ -1,2 +1,35 @@
 # response code 301-308 (permanent)/302-303-307 (temporary)
 
+from flask import Flask, redirect, url_for
+import time
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Welcome to the Home Page!</h1>"
+
+@app.route("/pass")
+def passed():
+    return "<h1>Congratz, You've passed!!</h1>"
+
+@app.route("/fail")
+def failed():
+    return "<h1>Sorry, You've failed!!</h1>"
+
+
+@app.route("/score/<name>/<int:num")
+def score(name, num):
+    if num <30:
+        time.sleep (1)
+        # redirect user to page "fail"
+        return redirect (url_for("failed"))
+    else:
+        time.sleep (1)
+        # redirect user to page "pass"
+        return redirect (url_for("passed"))
+        pass
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
